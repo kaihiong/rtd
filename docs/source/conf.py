d56cacf08c95d5834f1403d77748662b2f5fa44f
kaihiong <ounkaihiong@hotmail.com>
2022-10-11 16:14:55 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Kai Machine Learning Laboratory'
+copyright = '2022, Kai Hiong'
+author = 'Kai Hiong'
 
 release = '0.1'
 version = '0.1.0'
